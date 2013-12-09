0ffd2c2b5a396085c7d347694b82c9ee24d909e0
Robo-Raiders3681 <guy.e.oron@gmail.com>
2013-12-09 07:06:13 +0000

    second commit

---
+++ src/edu/wpi/first/wpilibj/templates/Index.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Index
+{
+    //PWM
+    final int LEFT_DRIVE_MOTOR = 1;
+    final int RIGHT_DRIVE_MOTOR = 2;
+    static final int LIFT_MOTOR = 3;
+    
+    //GPIO
+    final int LOWER_LIMIT = 1;
+    final int UPPER_LIMIT = 2;
+    final int PRESSURE_SWITCH = 3;
+    
+    //analog input
+    final int ANGLE = 1;
+    
+    //relay
+    final int COMPRESS_RELAY = 1;
+    final int FIRE_RELAY = 2;
+}
